fa623eb2f0290c54b008670c88a4672f10003770
madhu <madhukarr@ipsgsystems.com>
2017-02-06 18:50:37 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.*;
 
 //@@@System.out.println("GCE testing");
+
 @RestController
 public class GreetingController { 
     private static final String template = "Hello, %s!";
